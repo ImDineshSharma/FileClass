bf4b674e1913bafd865e24f2b3a56ebaab773ad4
Dinesh Sharma <39302151+ImDineshSharma@users.noreply.github.com>
2019-09-06 19:25:52 +0530

    Create Find.java

---
+++ Find.java
@@ -0,0 +1,17 @@
+// this is how to use this class to find a perticular file in your hard drive
+
+public class Find{
+   public static void main(String[] args){
+ 
+       File x = new File("F:\\PEND DRIVE\\TODAY.CSV"); // i am checking it in my hard drive for TODAY file whether or not it exists
+    	 
+    	 if(x.exists())
+    		 System.out.println(x.getName() + " exists!!");
+    	 else
+    		 System.out.println("this file does not exists");
+        
+    	 
+     }     
+}
+   }
+}
